c8fcfdce3abfaa6938efdb4c20ccffd7638f65fe
Lewis Goldenberg <lewisgoldenberg.0@gmail.com>
2025-06-09 21:21:52 -0400

    small html, file reordering

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,6 @@ import {Router} from 'express'
 import path from 'path';
 const router = Router();
 router.get('/', (req, res) => {
-    res.sendFile(path.resolve('static/home.html'))
+    res.sendFile(path.resolve('index.html'))
 })
 export default router;
